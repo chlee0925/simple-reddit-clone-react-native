cffe86bd5bc3fadf8bcf76114c5431acc7b4cca9
chlee0925 <chlee0925@gmail.com>
2018-03-01 04:40:11 +0800

    Add tests for component methods

--- components/topiclist/topiclist.test.js
+++ components/topiclist/topiclist.test.js
@@ -6,6 +6,59 @@ import renderer from 'react-test-renderer';
 it('renders TopicListScreen Component', () => {
     const screen = renderer.create(
         <TopicListScreen />
-    ).toJSON();
-    expect(screen).toMatchSnapshot();
+    );
+    expect(screen.toJSON()).toMatchSnapshot();
+
+    const instance = screen.getInstance();
+    expect(instance.state.data.length).toEqual(3);
+    expect(instance.state.data[0].key).toEqual(2);
+    expect(instance.state.data[1].key).toEqual(3);
+    expect(instance.state.data[2].key).toEqual(1);
+});
+
+it('test upvote', () => {
+    const instance = renderer.create(
+        <TopicListScreen />
+    ).getInstance();
+
+    expect(instance.state.data[1].key).toEqual(3);
+    instance.upvote(1);
+    expect(instance.state.data[1].key).toEqual(3);
+    instance.upvote(1);
+    expect(instance.state.data[1].key).toEqual(2);
+    expect(instance.state.data[0]).toEqual(
+        {
+            downvote: 4,
+            key: 3,
+            topic: 'Best ways to bargain with sellers in Carousell',
+            upvote: 3,
+        }
+    );
+});
+
+it('test downvote', () => {
+    const instance = renderer.create(
+        <TopicListScreen />
+    ).getInstance();
+
+    const before = instance.state.data[0].downvote;
+    instance.downvote(0);
+    expect(instance.state.data[0].downvote).toEqual(before + 1);
+});
+
+it('test createTopic', () => {
+    const instance = renderer.create(
+        <TopicListScreen />
+    ).getInstance();
+
+    instance.createTopic('test topic');
+    expect(instance.state.data.length).toEqual(4);
+    expect(instance.state.data[3]).toEqual(
+        {
+            downvote: 0,
+            key: 4,
+            topic: 'test topic',
+            upvote: 0,
+        }
+    );
 });
