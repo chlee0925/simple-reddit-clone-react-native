720eed82adc065ffd9bf4ac7cb803f43f66ef447
chlee0925 <chlee0925@gmail.com>
2018-02-28 21:12:22 +0800

    Add navigator

---
+++ components/topiclist/topiclist.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export class TopicListScreen extends React.Component {
+    render(){
+        return (<Text>topiclist</Text>)
+    }
+}
